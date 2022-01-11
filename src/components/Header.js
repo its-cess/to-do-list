68abbcf2d5b05cad5669f881e462c150b274af4e
its-cess <torocecily@gmail.com>
2022-01-11 16:07:57 -0500

    created state for adding a new item. logging value of new item each time state is updated. added click to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,44 @@
 import React from "react";
 
 export default function Header() {
+    const [inputText, setInputText] = React.useState(
+        {
+            newItem: ""
+        }
+    )
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setInputText(prevInputText => {
+            return {
+                ...prevInputText,
+                [name]: value
+            }
+        })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log("clicked")
+        //click working, need to create list and conditionally render 
+        //to App() when added.
+    }
+
     return (
-        <h1>To-Do:</h1>
+        <div>
+            <h1>To-Do:</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    className="input-box"
+                    type="text"
+                    placeholder="Add Item"
+                    name="newItem"
+                    value={inputText.newItem}
+                    onChange={handleChange}
+                >
+                </input>
+                <button className="add-btn"><i class="btn-icon fas fa-plus"></i></button>
+            </form>
+        </div>
     )
 }
