ea89643b917986f11ff81a32e705bb1688ae55fc
its-cess <torocecily@gmail.com>
2022-01-14 14:15:56 -0500

    submit item on enter push

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,68 +7,69 @@ const [inputText, setInputText] = React.useState(
             isDone: false
         }
     )
-    function handleChange(event) {
-        const {name, value} = event.target
-        setInputText(prevInputText => {
-            return {
-                ...prevInputText,
-                [name]: value
-            }
-        })
-    }
+function handleChange(event) {
+    const {name, value} = event.target
+    console.log(event.target)
+    setInputText(prevInputText => {
+        return {
+            ...prevInputText,
+            [name]: value
+        }
+    })
+}
 
 const [toDoItem, setToDoItem] = React.useState(
     JSON.parse(localStorage.getItem("toDoItem")) || [])
  
 React.useEffect (() => {
     localStorage.setItem("toDoItem", JSON.stringify(toDoItem))}, [toDoItem])
-    
+
 const currentToDoItem = inputText.newItem
 
 
-    function addItem(event) { 
-       setToDoItem(prevToDoItem => {
-           return [...prevToDoItem, currentToDoItem]
-            }, inputText.newItem = ""
-        )}
-    
-        const itemElements = toDoItem.map(item => 
-            <li 
-                value={inputText.newItem} 
-                isDone={inputText.isDone}
-            >
-                {item}
-            </li>)
+function addItem(event) { 
+    setToDoItem(prevToDoItem => {
+        return [...prevToDoItem, currentToDoItem]
+        }, inputText.newItem = ""
+    )}
+   
+const itemElements = toDoItem.map(item => 
+    <li 
+        value={inputText.newItem} 
+        isDone={inputText.isDone}
+    >
+        {item}
+    </li>)
 
-    return (
-        <main>
-        <h1>To-Do:</h1>
-        <div className="form-container">
-            <form>
-                <input
-                    className="input-box"
-                    type="text"
-                    placeholder="Add Item"
-                    name="newItem"
-                    value={inputText.newItem}
-                    onChange={handleChange}   
-                >
-                </input>
-            </form> 
-                <button 
-                    type="submit"
-                    className="submitButton"
-                    onClick={addItem}
-                >
-                    Add To List
-                </button>
-        </div>  
+return (
+<main>
+    <h1>To-Do:</h1>
+    <div className="form-container">
+        <form onSubmit={addItem}>
+             <input
+                className="input-box"
+                type="text"
+                placeholder="Add Item"
+                name="newItem"
+                value={inputText.newItem}
+                onChange={handleChange}
+            >
+            </input>
+        </form> 
+            <button 
+                type="submit"
+                className="submitButton"
+                onClick={addItem}
+            >
+                Add To List
+            </button>
+    </div>  
         
-         <div className="list-container">
-            <ul>
-                <li>{itemElements}</li>
-            </ul>
-         </div> 
-    </main>                 
-    )
+        <div className="list-container">
+        <ul>
+            <li>{itemElements}</li>
+        </ul>
+        </div> 
+</main>                 
+)
 }
