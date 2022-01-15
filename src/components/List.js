2e1f3bdc0b067a919c85e07931edf2d65609c578
its-cess <torocecily@gmail.com>
2022-01-14 21:36:34 -0500

    setup onclick for boxes. conditional rendering of empty or checked square.

--- src/components/List.js
+++ src/components/List.js
@@ -6,12 +6,18 @@ export default function List(props) {
     function toggleIsCompleted() {
         setIsCompleted(prevIsCompleted => !prevIsCompleted)
     }
-    
+
     return (
-        <div onClick={toggleIsCompleted}>
-            <li 
-                className={isCompleted ? "green" : ""}
-            >
+        <div 
+            className="done-box"
+            onClick={toggleIsCompleted}>
+                {
+                    isCompleted ? 
+                    <i className="far fa-check-square check-square"></i> 
+                    : 
+                    <i className="far fa-square empty-square"></i>
+                }
+            <li className={isCompleted ? "done" : ""}>
                 {props.value}
             </li>
         </div>
