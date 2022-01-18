e88b6986d227aec444e3798b76a97a17e16fd47f
its-cess <torocecily@gmail.com>
2022-01-17 20:38:05 -0500

    removed extra code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from "react/cjs/react.production.min";
 import List from "./components/List";
 
 export default function App() {
